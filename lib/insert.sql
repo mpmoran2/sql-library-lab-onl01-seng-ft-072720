9f1902e3624ba7f7efd646927a6cf7b782850fdc
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-26 01:27:07 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -14,6 +14,6 @@ INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Mizuki 
 
 INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Anita Blake", "motto5", "human", 2), (6,"Jean-Claude", "motto6", "vampire", 2), (7,"Edward", "motto7", "human", 2), (8,"Richard Zeeman", "motto8","werewolf", 2);
 
-INSERT INTO characters_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
-INSERT INTO characters_books(id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
+INSERT INTO character_books(id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
