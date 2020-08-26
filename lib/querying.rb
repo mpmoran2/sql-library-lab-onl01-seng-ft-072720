d0382180b002fa48471a1e5a6224987cdc855f83
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-26 01:15:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,31 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books
+  "SELECT books.title, books.year 
+  FROM books
   WHERE books.series_id = 1
   ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto FROM characters
+  "SELECT characters.name, characters.motto 
+  FROM characters
   ORDER BY characters.motto LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) FROM characters
-  GROUP BY characters.species ORDER BY COUNT(characters.species) DESC LIMIT 1;"
+  "SELECT characters.species, COUNT(characters.species) 
+  FROM characters
+  GROUP BY characters.species 
+  ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors
-  LEFT JOIN series ON authors.id = series.author_id
-  JOIN subgenres ON series.subgenre_id = subgenres.id;"
+  "SELECT authors.name, subgenres.name 
+  FROM authors
+  LEFT JOIN series 
+  ON authors.id = series.author_id
+  JOIN subgenres 
+  ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
